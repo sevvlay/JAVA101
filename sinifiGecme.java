3c03b2b44d466ef9546a0ee83f67df0d95d73fc9
sevvlay <160162299+sevvlay@users.noreply.github.com>
2024-08-07 16:03:04 +0300

    Add files via upload

--- sinifiGecme.java
+++ sinifiGecme.java
@@ -2,42 +2,54 @@ import java.util.Scanner;
 
 public class sinifiGecme {
     public static void main(String[] args) {
+        int mat,fizik,turkce,kimya,muzik;
+        int dersSayisi = 5;
+        Scanner input = new Scanner(System.in);
 
-    int mat, fizik, kimya, turkce, tarih, muzik;
+        System.out.print("Matematik notunuz: ");
+        mat = input.nextInt();
+        if(0>mat || mat>100){
+            mat = 0;
+            dersSayisi--;
+        }
 
-    Scanner input = new Scanner(System.in);
+        System.out.print("Fizik notunuz: ");
+        fizik = input.nextInt();
+        if(0>fizik || fizik>100){
+            fizik = 0;
+            dersSayisi--;
+        }
 
-    System.out.print("Matematik notunuz:");
-    mat = input.nextInt();
+        System.out.print("Türkçe notunuz: ");
+        turkce = input.nextInt();
+        if(0>turkce || turkce>100){
+            turkce = 0;
+            dersSayisi--;
+        }
 
-    System.out.print("Fizik notunuz:");
-    fizik = input.nextInt();
+        System.out.print("Kimya notunuz: ");
+        kimya = input.nextInt();
+        if(0>kimya || kimya>100){
+            kimya = 0;
+            dersSayisi--;
+        }
 
-    System.out.print("Kimya notunuz:");
-    kimya = input.nextInt();
+        System.out.print("Müzik notunuz: ");
+        muzik = input.nextInt();
+        if(0>muzik || muzik>100){
+            muzik = 0;
+            dersSayisi--;
+        }
 
-    System.out.print("Turkce notunuz:");
-    turkce = input.nextInt();
+        double average = (double) (mat + fizik + turkce + kimya + muzik) /dersSayisi;
 
-    System.out.print("Tarih notunuz:");
-    tarih = input.nextInt();
-
-    System.out.print("Muzik notunuz:");
-    muzik = input.nextInt();
-
-    double toplam = (mat + fizik + kimya + turkce + tarih + muzik);
-    double ortalama = toplam/6;
-
-    String gecmeDurumu = (ortalama > 60) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
-
-    System.out.println(gecmeDurumu);
-
-
-
-
-
-
-
-
+        if(average<55) {
+            System.out.println("Ortalamanız: " + average);
+            System.out.print("Maalesef sınıfta kaldınız.");
+        }
+        else{
+            System.out.println("Ortalamanız: " + average);
+            System.out.print("Sınıfı geçtiniz!");
+        }
     }
 }
